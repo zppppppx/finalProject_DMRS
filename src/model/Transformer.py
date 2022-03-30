ee5845370847d617f6ae441c916fc1a86dabb553
zppppppx <3180105152@zju.edu.cn>
2022-03-30 21:36:32 +0800

    Update transformer.

--- src/model/Transformer.py
+++ src/model/Transformer.py
@@ -30,7 +30,7 @@ class GELU(nn.Module):
         super(GELU, self).__init__()
 
     def forward(self, x):
-        return 0.5*x*(1+F.tanh(torch.sqrt(2/np.pi)*(x+0.044715*torch.pow(x,3))))
+        return 0.5*x*(1+torch.tanh(torch.sqrt(torch.tensor(2/np.pi))*(x+0.044715*torch.pow(x,3))))
 
 
 class MultiHead(nn.Module):
@@ -49,7 +49,7 @@ class MultiHead(nn.Module):
     Return:
         attention: a probability matrix
     """
-    def __init__(self, modelDim, h=4):
+    def __init__(self, modelDim, h=2):
         super(MultiHead, self).__init__()
         self.h = h
         headDim = modelDim//h
@@ -57,9 +57,14 @@ class MultiHead(nn.Module):
         self.head_V = nn.ModuleList([nn.Linear(modelDim, headDim) for _ in range(h)])
         self.head_K = nn.ModuleList([nn.Linear(modelDim, headDim) for _ in range(h)])
         self.head_Q = nn.ModuleList([nn.Linear(modelDim, headDim) for _ in range(h)])
+        map(initialize_weight, self.head_V)
+        map(initialize_weight, self.head_K)
+        map(initialize_weight, self.head_Q)
         self.W_o = nn.Linear(modelDim, modelDim)
 
-    def forward(self, V, K, Q):
+    def forward(self, V, K, Q, cache=None):
+        if cache is not None and 'encdec_k' in cache:
+            V, K = cache['encdec_v'], cache['encdec_k']
         attention = self._multiheadAttention(V, K, Q, self.head_V, self.head_K, self.head_Q,
                                              self.h, self.scale_factor, self.W_o)
         return attention
@@ -107,15 +112,16 @@ class MultiHead(nn.Module):
 
 
 class FeedForward(nn.Module):
-    def __init__(self, in_size, filter_size, dropout=0.2):
+    def __init__(self, hidden_size, filter_size, dropout=0.2):
         super(FeedForward, self).__init__()
         self.feedforward = nn.Sequential(
-            nn.Linear(in_size, filter_size),
+            nn.Linear(hidden_size, filter_size),
             GELU(),
             nn.Dropout(dropout),
-            nn.Linear(filter_size, in_size)
+            nn.Linear(filter_size, hidden_size)
         )
-        initialize_weight(self.feedforward)
+        initialize_weight(self.feedforward[0])
+        initialize_weight(self.feedforward[-1])
 
     def forward(self, x):
         return self.feedforward(x)
@@ -126,7 +132,7 @@ class EncoderLayer(nn.Module):
     Performing as the encoder layer, where we get rid of the masking stage because we don't need care about the actual order.
 
     Args(init):
-        in_size: the dimension of the input feature. (Here we don't need to rearrange the matrix.)
+        hidden_size: the dimension of the input feature. (Here we don't need to rearrange the matrix.)
         filter_size: the size of the linear layer.
         dropout: the dropout rate, for better generalization.
 
@@ -136,14 +142,14 @@ class EncoderLayer(nn.Module):
     Return:
         x: final result after the res block.
     """
-    def __init__(self, in_size, filter_size, dropout=0.2):
+    def __init__(self, hidden_size, filter_size, dropout=0.2):
         super(EncoderLayer, self).__init__()
-        self.attention_norm = nn.LayerNorm(in_size, eps=1e-7)
-        self.attention = MultiHead(in_size)
+        self.attention_norm = nn.LayerNorm(hidden_size, eps=1e-7)
+        self.attention = MultiHead(hidden_size)
         self.attention_dropout = nn.Dropout(dropout)
         
-        self.ffn_norm = nn.LayerNorm(in_size, eps=1e-7)
-        self.ffn = FeedForward(in_size, filter_size, dropout)
+        self.ffn_norm = nn.LayerNorm(hidden_size, eps=1e-7)
+        self.ffn = FeedForward(hidden_size, filter_size, dropout)
         self.ffn_dropout = nn.Dropout(dropout)
 
     def forward(self, x):
@@ -167,11 +173,11 @@ class EncoderLayer(nn.Module):
 
 
 class Encoder(nn.Module):
-    def __init__(self, in_size, filter_size, dropout=0.2, n_layers=1):
+    def __init__(self, hidden_size, filter_size, dropout=0.2, n_layers=1):
         super(Encoder, self).__init__()
-        encoders = [EncoderLayer(in_size, filter_size, dropout) for _ in range(n_layers)]
+        encoders = [EncoderLayer(hidden_size, filter_size, dropout) for _ in range(n_layers)]
         self.encoders = nn.ModuleList(encoders)
-        self.final_norm = nn.LayerNorm(in_size)
+        self.final_norm = nn.LayerNorm(hidden_size)
 
     def forward(self, x):
         y = x
@@ -182,8 +188,124 @@ class Encoder(nn.Module):
 
 
 class DecoderLayer(nn.Module):
-    def __init__(self):
+    """
+    Performing as the decoder layer, where we get rid of the masking stage because we don't need care about the actual order.
+
+    Args(init):
+        hidden_size: the dimension of the input feature. (Here we don't need to rearrange the matrix.)
+        filter_size: the size of the linear layer.
+        dropout: the dropout rate, for better generalization.
+
+    Args(forward):
+        x: the pending data
+        enc_output: the output of the encoder 
+        cache: 
+
+    """
+    def __init__(self,  hidden_size, filter_size, dropout=0.2):
         super(DecoderLayer, self).__init__()
+        self.self_attention_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+        self.self_attention = MultiHead(hidden_size)
+        self.self_attention_dropout = nn.Dropout(dropout)
+
+        self.enc_dec_attention_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+        self.enc_dec_attention = MultiHead(hidden_size)
+        self.enc_dec_attention_dropout = nn.Dropout(dropout)
+
+        self.ffn_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+        self.ffn = FeedForward(hidden_size, filter_size, dropout)
+        self.ffn_dropout = nn.Dropout(dropout)
+    
+    def forward(self, x, enc_output, cache):
+        ###############
+        # Add & Norm 1#
+        ###############
+        y = self.self_attention_norm(x)
+        y = self.self_attention(y, y, y)
+        y = self.self_attention_dropout(y)
+        x = x + y
+
+        ###############
+        # Add & Norm 2#
+        ###############
+        if enc_output is not None:
+            y = self.enc_dec_attention_norm(x)
+            y = self.enc_dec_attention(enc_output, enc_output, y, cache)
+            y = self.enc_dec_attention_dropout(y)
+            x = x + y
+
+        ################
+        # Feed Forward #
+        ################
+        y = self.ffn_norm(x)
+        y = self.ffn(y)
+        y = self.ffn_dropout(y)
+        x = x + y
+        return x
+
+
+class Decoder(nn.Module):
+    def __init__(self, hidden_size, filter_size, dropout, n_layers):
+        super(Decoder, self).__init__()
+
+        decoders = [DecoderLayer(hidden_size, filter_size, dropout) for _ in range(n_layers)]
+        self.layers = nn.ModuleList(decoders)
+
+        self.last_norm = nn.LayerNorm(hidden_size, eps=1e-6)
+
+    def forward(self, targets, enc_output, cache):
+        decoder_output = targets
+        for i, dec_layer in enumerate(self.layers):
+            layer_cache = None
+            if cache is not None:
+                if i not in cache:
+                    cache[i] = {}
+                layer_cache = cache[i]
+            decoder_output = dec_layer(decoder_output, enc_output, layer_cache)
+        return self.last_norm(decoder_output)
+
+
+class Transformer(nn.Module):
+    def __init__(self, f_in_size, f_out_size, 
+                n_layers=2,
+                hidden_size=256,
+                filter_size=512,
+                dropout=0.1,
+                has_input=True
+                ):
+        super(Transformer, self).__init__()
+        self.f_in_size = f_in_size
+        self.f_out_size = f_out_size
+        self.has_input = has_input
+
+        # Linear layer for input to encoder
+        self.input_layer = nn.Linear(f_in_size, hidden_size)
+
+        self.decoder = Decoder(hidden_size, filter_size, dropout, n_layers)
+        if  has_input:
+            self.encoder = Encoder(hidden_size, filter_size, dropout, n_layers)
+
+        # Linear layer for input to decoder
+        self.target_input_layer = nn.Linear(f_out_size, hidden_size)
+
+        # final linear layer
+        self.output_layer = nn.Linear(hidden_size, f_out_size)
+
+    def forward(self, inputs, targets):
+        enc_input = self.input_layer(inputs)
+        enc_output = None
+        if self.has_input:
+            enc_output = self._encode(enc_input)
+        return self._decode(targets, enc_output)
+        
+    
+    def _encode(self, inputs):
+        return self.encoder(inputs)
+
+    def _decode(self, targets, enc_output, cache=None):
+        decoder_input = self.target_input_layer(targets)
+        decoder_output = self.decoder(decoder_input, enc_output, cache)
+        return self.output_layer(decoder_output)
 
 
 if __name__ == "__main__":
@@ -192,15 +314,25 @@ if __name__ == "__main__":
     out = layernorm(n)
     print(out.shape)
 
-    Attention = MultiHead(192, 2)
-    v = torch.rand((32, 14, 192))
-    k = torch.rand((32, 14, 192))
-     
-    q = torch.rand((32, 14, 192))
+    Attention = MultiHead(96, 2)
+    encoder = Encoder(192, 384)
+    transformer = Transformer(96, 192, 1)
+    inputs = torch.rand((32, 14, 96))
+    targets = torch.rand((32, 14, 192))
 
-    att = Attention(v, k, q)
+
+    v = torch.rand((32, 14, 96))
+    k = torch.rand((32, 14, 96))
+     
+    q = torch.rand((32, 14, 96))
+
+    # att = Attention(v, k, q)
+    # enc_output = encoder(v)
+    out = transformer(inputs, targets)
     # att = Attention._attention(v, k, q, scale_factor=1)
-    print(att.shape)
+    # print(att.shape)
+    # print(enc_output.shape)
+    print(out.shape)
     
     
 
